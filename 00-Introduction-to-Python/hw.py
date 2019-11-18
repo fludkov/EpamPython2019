a38695288a80873692605e2f4c925922b8f30a6c
valerii_fludkov <valerii_fludkov@epam.com>
2019-11-18 10:17:01 +0300

    Completed Homework

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -11,20 +11,10 @@
 def is_permutation(a: str, b: str) -> bool:
     """This function accepts on entry two string values and returns
     a boolean value if 'a' string is permutation of 'b' string"""
-    a_list = list(a)
-    b_list = list(b)
-    for i in a:
-        if i in b_list:
-            a_list.remove(i)
-            b_list.remove(i)
-        else:
-            return False
-    if a_list == b_list:
-        return True
-    else:
-        return False
-    # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
 
+    return True if sorted(a) == sorted(b) else False
+
+    # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
 
 assert is_permutation('baba', 'abab')
 assert is_permutation('abbba', 'abab')
